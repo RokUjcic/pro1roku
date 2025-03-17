ef32ab727e0166b973f5327316db1c5fab6c3708
RokUjcic <rokujcic12045@gmail.com>
2025-03-17 13:55:17 +0100

    Add files via upload

---
+++ Binarno_isknaje_naloga1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarnoIskanje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] števila = { 5, 7, 7, 8, 10 };
+            int cilj = 8;
+
+            int[] rezultat = NajdiPrviInZadnjiPolozaj(števila, cilj);
+            Console.WriteLine("Prvi in zadnji položaj števila " + cilj + " sta: [" + rezultat[0] + "," + rezultat[1] + "]");
+            Console.ReadLine();
+        }
+
+        static int[] NajdiPrviInZadnjiPolozaj(int[] seznam, int cilj)
+        {
+            int prvi = BinarnoIskanje(seznam, cilj, true);
+            int zadnji = BinarnoIskanje(seznam, cilj, false);
+            return new int[] { prvi, zadnji };
+        }
+
+        static int BinarnoIskanje(int[] seznam, int cilj, bool Prvi)
+        {
+            int levo = 0;
+            int desno = seznam.Length - 1;
+            int rezultat = -1;
+
+            while(levo <= desno)
+            {
+                int sredina = levo + (desno - levo) / 2;
+
+                if (seznam[sredina] == cilj)
+                {
+                    rezultat = sredina;
+                    if (Prvi)
+                        desno = sredina - 1;
+                    else
+                        levo = sredina + 1;
+                }
+                else if (seznam[sredina] < cilj)
+                {
+                    levo = sredina + 1;
+                }
+                else
+                {
+                    desno = sredina - 1;
+                }
+            }
+            return rezultat;
+        }
+    }
+}
